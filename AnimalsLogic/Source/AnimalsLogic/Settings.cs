205b55b748f65282d10740a31045177b7fbd5dc9
Quicksilver Fox <QuicksilverFox@ignis.name>
2017-07-26 22:11:14 +0300

    More logic. Option to nerf wildeness penalty on training.

--- AnimalsLogic/Source/AnimalsLogic/Settings.cs
+++ AnimalsLogic/Source/AnimalsLogic/Settings.cs
@@ -6,13 +6,17 @@ namespace AnimalsLogic
     class Settings : ModSettings
     {
         public static bool prevent_eating_stuff = true;
+        public static float training_wildeness_effect_to = 0;
 
         public static void DoSettingsWindowContents(Rect inRect)
         {
             Listing_Standard listing_Standard = new Listing_Standard();
             listing_Standard.Begin(inRect);
 
-            listing_Standard.CheckboxLabeled("Prevent animals from eating random stuff", ref Settings.prevent_eating_stuff);
+            listing_Standard.CheckboxLabeled("Prevent animals from eating random stuff", ref prevent_eating_stuff);
+
+            listing_Standard.Label("Wildeness effect on training");
+            training_wildeness_effect_to = 1 - listing_Standard.Slider(1 - training_wildeness_effect_to, 0, 1);
 
             listing_Standard.End();
         }
@@ -20,7 +24,8 @@ namespace AnimalsLogic
         public override void ExposeData()
         {
             base.ExposeData();
-            Scribe_Values.Look<bool>(ref Settings.prevent_eating_stuff, "prevent_eating_stuff", false, false);
+            Scribe_Values.Look<bool>(ref prevent_eating_stuff, "prevent_eating_stuff", true, false);
+            Scribe_Values.Look<float>(ref training_wildeness_effect_to, "training_wildeness_effect_to", 0, false);
         }
     }
 }
---
+++ AnimalsLogic/Source/AnimalsLogic/TrainingWildnessFactor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Harmony;
+using RimWorld;
+using Verse;
+using Verse.AI;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+
+namespace AnimalsLogic
+{
+    class TrainingWildnessFactor
+    {
+        [HarmonyPatch]
+        public static class Toils_Interpersonal_TryTrain_Patch
+        {
+            static MethodInfo TargetMethod()
+            {
+                return typeof(Toils_Interpersonal).GetNestedType("<TryTrain>c__AnonStorey26F", AccessTools.all).GetMethod("<>m__E1", AccessTools.all);
+            }
+
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                MethodInfo LerpDouble = typeof(GenMath).GetMethod("LerpDouble");
+                var codes = new List<CodeInstruction>(instructions);
+                for (int i = 0; i < codes.Count; i++)
+                {
+                    if (codes[i].opcode == OpCodes.Call)
+                    {
+                        MethodInfo operand = (MethodInfo)codes[i].operand;
+                        if (operand == LerpDouble)
+                        {
+                            //codes[i - 5] = new CodeInstruction(OpCodes.Ldsfld, typeof(Settings).GetField("training_wildeness_effect_from"));
+                            codes[i - 4] = new CodeInstruction(OpCodes.Ldsfld, typeof(Settings).GetField("training_wildeness_effect_to"));
+                            break;
+                        }
+                    }
+                }
+
+                return codes.AsEnumerable();
+            }
+        }
+    }
+}

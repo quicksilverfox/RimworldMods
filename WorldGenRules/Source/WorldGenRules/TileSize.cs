8ff61bd3639a8ef844a045194cd726d9f30c9994
Quicksilver Fox <QuicksilverFox@ignis.name>
2017-08-14 15:33:13 +0300

    WG flickering fix for small worlds.

--- WorldGenRules/Source/WorldGenRules/TileSize.cs
+++ WorldGenRules/Source/WorldGenRules/TileSize.cs
@@ -2,7 +2,10 @@
 using RimWorld;
 using RimWorld.Planet;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
+using System.Reflection.Emit;
 using UnityEngine;
 using Verse;
 
@@ -51,12 +54,129 @@ namespace WorldGenRules
             static void Postfix(ref Rect __state, ref Page_CreateWorldParams __instance)
             {
                 GUI.BeginGroup(__state);
-                float num = 250; // magic numba!
+                float num = 260; // magic numba!
                 Widgets.Label(new Rect(0f, num, 200f, 30f), "Planet Size");
                 Rect rect7 = new Rect(200f, num, 200f, 30f);
                 subcount = Mathf.RoundToInt(Widgets.HorizontalSlider(rect7, subcount, 6f, 11f, true, null, "Small", "Large", 1f));
                 //Settings.subdivisionsCount = Mathf.RoundToInt(Widgets.HorizontalSlider(rect7, Settings.subdivisionsCount, 6f, 11f, true, null, "Small", "Large", 1f));
                 GUI.EndGroup();
+            }
+        }
+
+        // anti-fluckering stuff - changing world layers distance to not overlap with sphere
+
+
+        [HarmonyPatch]
+        static class WorldLayer_Hills_Regenerate_Patch
+        {
+            static MethodInfo TargetMethod()
+            {
+                var toils_inner = typeof(WorldLayer_Hills).GetNestedTypes(AccessTools.all);
+                foreach (var inner_class in toils_inner)
+                {
+                    if (!inner_class.Name.Contains("<Regenerate>"))
+                        continue;
+
+                    var methods = inner_class.GetMethods(AccessTools.all);
+                    foreach (var method in methods)
+                    {
+                        if (method.Name.Contains("MoveNext"))
+                            return method;
+                    }
+                }
+                Log.Error("WorldGenRules.WorldLayer_Hills is unable to detect WorldLayer_Roads inner method.");
+                return null;
+            }
+
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                var codes = new List<CodeInstruction>(instructions);
+                for (int i = 0; i < codes.Count; i++)
+                {
+                    // WorldRendererUtility.PrintQuadTangentialToPlanet(pos, origPos, WorldLayer_Hills.BaseSizeRange.RandomInRange * grid.averageTileSize, 0.005f, subMesh, false, true, false);
+                    if (codes[i].opcode == OpCodes.Ldc_R4 && (float)codes[i].operand == 0.005f)
+                    {
+                        codes[i].operand = 0.1f;
+                        break;
+                    }
+                }
+                return codes.AsEnumerable();
+            }
+        }
+
+        [HarmonyPatch(typeof(WorldLayer_Rivers), "FinalizePoint", null)]
+        static class WorldLayer_Rivers_FinalizePoint_Patch
+        {
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                var codes = new List<CodeInstruction>(instructions);
+                for (int i = 0; i < codes.Count; i++)
+                {
+                    // return inp + inp.normalized * 0.008f;
+                    if (codes[i].opcode == OpCodes.Ldc_R4 && (float)codes[i].operand == 0.008f)
+                    {
+                        codes[i].operand = 0.11f;
+                        break;
+                    }
+                }
+                return codes.AsEnumerable();
+            }
+        }
+
+        [HarmonyPatch(typeof(WorldLayer_Roads), "FinalizePoint", null)]
+        static class WorldLayer_Roads_FinalizePoint_Patch
+        {
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                var codes = new List<CodeInstruction>(instructions);
+                for (int i = 0; i < codes.Count; i++)
+                {
+                    // return inp + inp.normalized * 0.012f;
+                    if (codes[i].opcode == OpCodes.Ldc_R4 && (float)codes[i].operand == 0.012f)
+                    {
+                        codes[i].operand = 0.12f;
+                        break;
+                    }
+                }
+                return codes.AsEnumerable();
+            }
+        }
+
+        [HarmonyPatch]
+        static class WorldLayer_SingleTile_Regenerate_Patch
+        {
+            static MethodInfo TargetMethod()
+            {
+                var toils_inner = typeof(WorldLayer_SingleTile).GetNestedTypes(AccessTools.all);
+                foreach (var inner_class in toils_inner)
+                {
+                    if (!inner_class.Name.Contains("<Regenerate>"))
+                        continue;
+
+                    var methods = inner_class.GetMethods(AccessTools.all);
+                    foreach (var method in methods)
+                    {
+                        if (method.Name.Contains("MoveNext"))
+                            return method;
+                    }
+                }
+                Log.Error("WorldGenRules.WorldLayer_SingleTile is unable to detect WorldLayer_Roads inner method.");
+                return null;
+            }
+
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                var codes = new List<CodeInstruction>(instructions);
+                for (int i = 0; i < codes.Count; i++)
+                {
+                    // subMesh.verts.Add(this.verts[i] + this.verts[i].normalized * 0.012f);
+                    if (codes[i].opcode == OpCodes.Ldc_R4 && (float)codes[i].operand == 0.012f)
+                    {
+                        codes[i].operand = 0.12f;
+                        break;
+                    }
+                }
+                return codes.AsEnumerable();
             }
         }
     }

a7905bebaf2244cdee94c299b44dd91bf77d9dbc
Quicksilver Fox <QuicksilverFox@ignis.name>
2018-05-18 13:17:07 +0300

    Logistics rewritten from scratch.

--- Logistics/Source/Logistics/Logistics.cs
+++ Logistics/Source/Logistics/Logistics.cs
@@ -5,127 +5,113 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Reflection.Emit;
 using UnityEngine;
 using Verse;
 
 namespace Logistics
 {
     [StaticConstructorOnStartup]
-    class Logistics
+    class Logistics : Mod
     {
-        static Logistics()
+#pragma warning disable 0649
+        public static Settings Settings;
+#pragma warning restore 0649
+
+        public Logistics(ModContentPack content) : base(content)
         {
             var harmony = HarmonyInstance.Create("net.quicksilverfox.rimworld.mod.logistics");
             harmony.PatchAll(Assembly.GetExecutingAssembly());
+            base.GetSettings<Settings>();
+        }
+
+        public void Save()
+        {
+            LoadedModManager.GetMod<Logistics>().GetSettings<Settings>().Write();
+        }
+
+        public override string SettingsCategory()
+        {
+            return "Logistics";
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Settings.DoSettingsWindowContents(inRect);
         }
     }
 
     [HarmonyPatch(typeof(WorldPathGrid), "CalculatedCostAt", new Type[] { typeof(int), typeof(bool), typeof(float) })]
     static class WorldPathGrid_CalculatedCostAt_Patch
     {
-        // This is a pretty dirty way to go, since it prevents original function from invoking, as well as other mods which can use it. But it has too many inline modifications to use Prefix/Postfix pair or Transpiler.
-        static bool Prefix(ref int __result, int tile, bool perceivedStatic, float yearPercent = -1f)
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
         {
-            int num = 0;
-            Tile tile2 = Find.WorldGrid[tile];
-            if (tile2.biome.impassable || tile2.hilliness == Hilliness.Impassable) // Impassable Map Maker would override this in Postfix, and it won't be affected by this mod.
+            var codes = new List<CodeInstruction>(instructions);
+
+            // patching biome cost modifier to ignore snow penalty when temperature is above 0
+            for (int i = 0; i < codes.Count; i++)
             {
-                __result =  1000000;
-                return false;
+                //	IL_00e1: call int32 RimWorld.Planet.WorldPathGrid::CostFromTileHilliness(valuetype RimWorld.Planet.Hilliness)
+                if (codes[i].opcode == OpCodes.Call && codes[i].operand == typeof(WorldPathGrid).GetMethod("CostFromTileHilliness", AccessTools.all))
+                {
+                    codes.RemoveAt(i + 1); // removes [add]
+                    codes.InsertRange(i + 1, new List<CodeInstruction>() {
+                        // biomeCost already on stack
+                        // hillnessCost already on stack
+                        new CodeInstruction(OpCodes.Ldarg_0), // push tile id to stack
+                        new CodeInstruction(OpCodes.Ldarg_2), // push original value of yearPercent to stack
+                        new CodeInstruction(OpCodes.Call, typeof(WorldPathGrid_CalculatedCostAt_Patch).GetMethod(nameof(CheckSnowAndSettlementMods)))
+                        // now corrected value is on stack
+                    });
+
+                    break;
+                }
             }
 
-            Settlement settlement;
-            Faction playerFaction = Faction.OfPlayerSilentFail;
-            if (playerFaction != null && (settlement = Find.WorldObjects.SettlementAt(tile)) != null && (settlement.Faction == playerFaction || settlement.Visitable))
-            {
-                __result = 0; // Friendly or player-controlled settlements ignore all terrain costs. Home, sweet home!
-                return false;
-            }
-            if (yearPercent < 0f)
-            {
-                yearPercent = (float)DayOfYearAt0Long / 60f;
-            }
-            float num2;
-            float num3;
-            float num4;
-            float num5;
-            float num6;
-            float num7;
-            SeasonUtility.GetSeason(yearPercent, Find.WorldGrid.LongLatOf(tile).y, out num2, out num3, out num4, out num5, out num6, out num7);
-            num += Mathf.RoundToInt((float)tile2.biome.pathCost_spring * num2 + (float)tile2.biome.pathCost_summer * num3 + (float)tile2.biome.pathCost_fall * num4 + (float)tile2.biome.pathCost_winter * num5 + (float)tile2.biome.pathCost_summer * num6 + (float)tile2.biome.pathCost_winter * num7);
-            __result = Mathf.RoundToInt((num + CostFromTileHilliness(tile2.hilliness)) * GetSettlementMoveModifier(tile));
-            
-            // Old calculations.
-            //bool calculateForCurrentTime = yearPercent == -1f;
-            //if (yearPercent < 0f)
-            //{
-            //    yearPercent = (float)DayOfYearAt0Long / 60f;
-            //}
-            //float num2 = yearPercent;
-            //if (Find.WorldGrid.LongLatOf(tile).y < 0f)
-            //{
-            //    num2 = (num2 + 0.5f) % 1f;
-            //}
+            return codes.AsEnumerable();
+        }
 
-            //// If we are calculating path for now, don't apply snow penalty if there are... Well, you know, no snow.
-            //if (calculateForCurrentTime && GenTemperature.GetTemperatureAtTile(tile) >= 0)
-            //{
-            //    num2 = 0.33f;
-            //}
-            //num += Mathf.RoundToInt(tile2.biome.pathCost.Evaluate(num2) * GetSettlementMoveModifier(tile));
-            //num += Mathf.RoundToInt(CostFromTileHilliness(tile2.hilliness) * GetSettlementMoveModifier(tile));
-            //__result = num;
+        public static int CheckSnowAndSettlementMods(int biomeCost, int hillnessCost, int tile, float yearPercent)
+        {
+            if (yearPercent == -1f) // -1 is used for current time, other values are for abstract calculations. Only flat modifier is applied fot abstracts.
+            {
+                // no snow - no seasonal penalty
+                if (Settings.snow_mod && GenTemperature.GetTemperatureAtTile(tile) > 0)
+                    biomeCost = Find.WorldGrid[tile].biome.pathCost_summer;
+            }
 
-            return false;
+            biomeCost = (int)(biomeCost * Settings.biome_time_modifier);
+            hillnessCost = (int)(hillnessCost * Settings.hillness_time_modifier);
+
+            // check settlement factor
+            if (Settings.settlement_mod)
+                return GetSettlementMoveModifier(tile, biomeCost + hillnessCost);
+
+            return biomeCost + hillnessCost;
         }
 
         // Even non-friendly settlement has some roads and you can use them.
-        private static float GetSettlementMoveModifier(int tile)
+        public static int GetSettlementMoveModifier(int tile, int calculatedCost)
         {
+            Settlement settlement = Find.WorldObjects.SettlementAt(tile);
+
+            if (Faction.OfPlayerSilentFail == null) // map generation stage - no change
+                return calculatedCost;
+
+            if (settlement != null && (settlement.Faction == Faction.OfPlayerSilentFail || settlement.Visitable))
+               return 0; // Friendly or player-controlled settlements ignore all terrain costs. Home, sweet home!
+
             float mod = 1f;
-            // Due to how settlements are handled, this stuff is vey CPU-heavy. Cut out for now.
-            //if (Find.WorldObjects.AnySettlementAt(tile))
-            //{
-            //    mod = Math.Min(0.3f, mod);
-            //}
-            //if (Find.WorldObjects.AnyDestroyedFactionBaseAt(tile))
-            //{
-            //    mod = Math.Min(0.4f, mod);
-            //}
-            //if (Find.WorldObjects.AnySettlementAtOrAdjacent(tile))
-            //{
-            //    mod = Math.Min(0.5f, mod);
-            //}
-            return mod;
-        }
 
-        // Copy/paste from WorldPathGrid due to private. Easier and faster than messing with reflection, even if not as safe.
-        private static int DayOfYearAt0Long
-        {
-            get
-            {
-                return GenDate.DayOfYear((long)GenTicks.TicksAbs, 0f);
-            }
-        }
+            if (settlement != null)
+                mod = Math.Min(0.5f, mod); // hostile settlements have half cost
 
-        // Copy/paste from WorldPathGrid due to private. Easier and faster than messing with reflection, even if not as safe.
-        private static int CostFromTileHilliness(Hilliness hilliness)
-        {
-            switch (hilliness)
-            {
-                case Hilliness.Flat:
-                    return 0;
-                case Hilliness.SmallHills:
-                    return 2000;
-                case Hilliness.LargeHills:
-                    return 6000;
-                case Hilliness.Mountainous:
-                    return 30000;
-                case Hilliness.Impassable:
-                    return 30000;
-                default:
-                    return 0;
-            }
+            // Due to how settlements are handled, this stuff is very CPU-heavy. Cut out for now.
+            //else if (Find.WorldObjects.AnySettlementAtOrAdjacent(tile))
+            //{
+            //    mod = Math.Min(0.35f, mod);
+            //}
+            return (int)(calculatedCost * mod);
         }
     }
 }
---
+++ Logistics/Source/Logistics/Settings.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using Verse;
+using System;
+
+namespace Logistics
+{
+    class Settings : ModSettings
+    {
+        public static bool settlement_mod = true;
+        public static bool snow_mod = true;
+        public static float biome_time_modifier = 1f;
+        public static float hillness_time_modifier = 1f;
+
+        public static void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listing_Standard = new Listing_Standard();
+            listing_Standard.Begin(inRect);
+
+            listing_Standard.CheckboxLabeled("Bypass terrain cost on friendly settlements", ref settlement_mod, "Settlement is considered friendly if you can visit it. Player-controlled settlements are always friendly.");
+            listing_Standard.CheckboxLabeled("Suppress winter penaly on tiles without snow", ref snow_mod, "Winter penalty will be applied only for tiles with negative temperature.");
+
+            listing_Standard.Label("Biome-added movement time factor. Current value: " + ((float)Math.Round(biome_time_modifier, 2)).ToStringPercent() + ".");
+            biome_time_modifier = listing_Standard.Slider((float)Math.Round(biome_time_modifier, 2), 0f, 2f);
+
+            listing_Standard.Label("Hillness-added movement time factor. Current value: " + ((float)Math.Round(hillness_time_modifier, 2)).ToStringPercent() + ".");
+            hillness_time_modifier = listing_Standard.Slider((float)Math.Round(hillness_time_modifier, 2), 0f, 2f);
+
+            listing_Standard.End();
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look<bool>(ref settlement_mod, "settlement_mod", true, false);
+            Scribe_Values.Look<bool>(ref snow_mod, "snow_mod", true, false);
+            Scribe_Values.Look<float>(ref biome_time_modifier, "biome_time_modifier", 1f, false);
+            Scribe_Values.Look<float>(ref hillness_time_modifier, "hillness_time_modifier", 1f, false);
+        }
+    }
+}

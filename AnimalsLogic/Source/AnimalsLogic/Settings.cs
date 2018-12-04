9d6e80c66e73d32fdd1b1aa9970a2f47d6d8a11f
Quicksilver Fox <QuicksilverFox@ignis.name>
2018-12-04 03:21:46 +0300

    Extended translation support for Animals Logic. Russian translation.

--- AnimalsLogic/Source/AnimalsLogic/Settings.cs
+++ AnimalsLogic/Source/AnimalsLogic/Settings.cs
@@ -22,20 +22,20 @@ namespace AnimalsLogic
             Listing_Standard listing_Standard = new Listing_Standard();
             listing_Standard.Begin(inRect);
 
-            listing_Standard.CheckboxLabeled("Prevent animals from eating random stuff", ref prevent_eating_stuff, "It mostly prevents your animals from eating drugs without a nutrition and stuff outside their allowed zones. They would still eat your food if it is in their allowed zone.");
-            listing_Standard.CheckboxLabeled("Predators hunting your pawns are hostile to all your faction", ref hostile_predators, "This does not change threat response of your pawns, it only makes them recognize a threat.");
+            listing_Standard.CheckboxLabeled("ALConfigCuriosityEatingLabel".Translate(), ref prevent_eating_stuff, "ALConfigCuriosityEatingTooltip".Translate());
+            listing_Standard.CheckboxLabeled("ALConfigHostilePredatorsLabel".Translate(), ref hostile_predators, "ALConfigHostilePredatorsTooltip".Translate());
             //listing_Standard.CheckboxLabeled("Wild animals eating your crops are hostile to all your faction", ref hostile_vermins, "This does not change threat response of your pawns, it only makes them recognize a threat. Targets designated for taming are ignored!");
-            listing_Standard.CheckboxLabeled("Convert eggs ruined by temperature into unfertilized chicken eggs", ref convert_ruined_eggs, "This does not affect already ruined eggs.");
-            listing_Standard.CheckboxLabeled("Convert any generic animal meat into chicken meat upon butchering", ref tastes_like_chicken, "This does not affect already butchered meat.");
-            listing_Standard.CheckboxLabeled("Medical alerts for animals", ref medical_alerts, "Shows right-hand alerts for when colony animals are injured, need rescuing or in critical medical condition.");
+            listing_Standard.CheckboxLabeled("ALConfigEggConversionLabel".Translate(), ref convert_ruined_eggs, "ALConfigEggConversionTooltip".Translate());
+            listing_Standard.CheckboxLabeled("ALConfigMeatConversionLabel".Translate(), ref tastes_like_chicken, "ALConfigMeatConversionTooltip".Translate());
+            listing_Standard.CheckboxLabeled("ALConfigMedicalAlertsLabel".Translate(), ref medical_alerts, "ALConfigMedicalAlertsTolltip".Translate());
 
-            listing_Standard.Label("Wildness threshold for tameness decay " + ((float)Math.Round(wildness_threshold_for_tameness_decay, 3) * 100).ToString() + "%. Vanilla: 10.1%.", -1, "Set to 100% to prevent losing tameness for all animals.");
+            listing_Standard.Label("ALConfigTamenessDecayThresholdLabel".Translate(((float)Math.Round(wildness_threshold_for_tameness_decay, 3) * 100).ToString()), -1, "ALConfigTamenessDecayThresholdTooltip".Translate());
             wildness_threshold_for_tameness_decay = listing_Standard.Slider(wildness_threshold_for_tameness_decay, 0f, 1f);
 
-            listing_Standard.Label("Training decay speed " + ((float)Math.Round(training_decay_factor, 3)).ToStringPercent() + ". Vanilla: 100%.", -1, "Set to 1% to make it as slow as possible.");
+            listing_Standard.Label("ALConfigTrainingDecaySpeedFactorLabel".Translate(((float)Math.Round(training_decay_factor, 3)).ToStringPercent()), -1, "ALConfigTrainingDecaySpeedFactorTooltip".Translate());
             training_decay_factor = listing_Standard.Slider(training_decay_factor, 0.01f, 2f);
             
-            listing_Standard.Label("Hauling MTB " + ((float)Math.Round(Math.Round(haul_mtb * 2) / 2f, 1)) + "h. Vanilla: 1.5h.", -1, "'MTB' stands for 'Mean Time Between'");
+            listing_Standard.Label("ALConfigHaulingMTBLabel".Translate(((float)Math.Round(Math.Round(haul_mtb * 4) / 4f, 2))), -1, "ALConfigHaulingMTBTooltip".Translate());
             haul_mtb = listing_Standard.Slider(haul_mtb, 0.0f, 3f);
             
             listing_Standard.End();

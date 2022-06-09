f02b5b376be3488446d66bae9f573407490f7759
Quicksilver Fox <ignis@ignis.name>
2022-06-09 16:14:54 +0300

    New addition for Animals Logic - now purchased eggs would hatch already tamed.

---
+++ AnimalsLogic/Source/AnimalsLogic/Patches/YouAreMine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace AnimalsLogic.Patches
+{
+    /*
+     * Makes any purchased eggs hatch already tamed.
+     */
+    class YouAreMine
+    {
+        [HarmonyPatch(typeof(ITrader), "GiveSoldThingToPlayer", new Type[] {  typeof(Thing), typeof(int), typeof(Pawn) })]
+        static class ITrader_GiveSoldThingToPlayer_Patch
+        {
+            static void Postfix(Thing toGive, int countToGive, Pawn playerNegotiator)
+            {
+                if (toGive?.TryGetComp<CompHatcher>() != null)
+                {
+                    toGive.TryGetComp<CompHatcher>().hatcheeFaction = Faction.OfPlayerSilentFail;
+                }
+            }
+        }
+    }
+}
